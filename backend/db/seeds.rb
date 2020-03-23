b8afbe76e5ceb0b586dc9f8c325953a6d820eb3f
GoTo15 <github email address>
2020-03-23 15:31:59 -0500

    Create model skeletons

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Ben')
+User.create(name: 'Lugh')
+
+Game.create(user_id: 1, score: 500)
+Game.create(user_id: 1, score: 450)
+Game.create(user_id: 1, score: 525)
+Game.create(user_id: 2, score: 500)
+Game.create(user_id: 2, score: 450)
+Game.create(user_id: 2, score: 350)
